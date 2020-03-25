fdf58e73f2b3028b679348833f4a73185f0eac01
Leonardo Rivaldo <leonardo.rivaldo@gmail.com>
2020-03-25 19:09:02 +0100

    add onpagechange callback
    this exposes the component page state

---
+++ index.js
@@ -0,0 +1,15 @@
+import template from './template.html';
+import controller from './controller';
+
+export default {
+  template,
+  controller,
+  controllerAs: 'vm',
+  bindings: {
+    collection: '<',
+    paginatedCollection: '=',
+    perPage: '<',
+    navigationLength: '<',
+    onPageChange: '&'
+  }
+};
